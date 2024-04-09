aa37e3c85480207adf02c43dc588a46956ee7a3f
Sree <isreekruthi@gmail.com>
2024-04-08 21:14:44 -0400

    Added Policyholder information
    Imported fields and methods from Policy class

---
+++ PolicyHolder.java
@@ -0,0 +1,159 @@
+public class PolicyHolder {
+   private String firstName;
+   private String lastName;
+   private int age;
+   private String smokingStatus;
+   private double height;
+   private double weight;
+   
+   /**
+    *
+    * No-arg constructor and default values
+    */
+    
+   public PolicyHolder() {
+      this.firstName = "";
+      this.lastName = "";
+      this.age = 0;
+      this.smokingStatus = "non-smoker";
+      this.height = 0;
+      this.weight = 0;
+   }
+   
+ /**
+    * Constructor accepting arguments
+    *
+    *@param firstName The first name of the policyholder.
+    *@param lastName The last name of the policyholder.
+    *@param age The age of the policyholder.
+    *@param smokingStatus The smoking status of the policyholder.
+    *@param height The height of the policyholder in inches.
+    *@param weight The weight of the policyholder in pounds.
+    */
+   public PolicyHolder(String firstName, String lastName, int age, String smokingStatus, double height, double weight) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.smokingStatus = smokingStatus;
+      this.height = height;
+      this.weight = weight;
+   }
+   
+     /**
+    * Gets the first name of the policyholder.
+    *
+    * @return the first name
+    */
+   public String getFirstName(){
+      return firstName;
+   }
+   
+   /**
+    * Sets the first name of the policyholder
+    *
+    * @param firstName the first name to set.
+    */
+   public void setFirstName(String fristName) {
+      this.firstName = firstName;
+   }
+   
+   /**
+    * Gets the last name of the policyholder
+    *
+    * @return the last name
+    */
+   public String getLastName() {
+      return lastName;
+   }
+   
+   /**
+    * Sets the last name of the policyholder
+    *
+    * @param lastName the last name to set
+    */
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+   
+   /**
+    * Gets the age of the policyholder
+    *
+    * @return the age
+    */
+   public int getAge() {
+      return age;
+   }
+   
+   /**
+    * Sets the age of the policyholder
+    *
+    * @param age the age to set
+    */
+   public void setAge(int age) {
+      this.age = age;
+   }
+   
+   /**
+    * Gets the smoking status of the policyholder
+    *
+    * @return the smoking status
+    */
+   public String getSmokingStatus() {
+      return smokingStatus;
+   }
+   
+   /**
+    * Sets the smoking status of the policyholder
+    *
+    * @param smokingStatus the smoking status is set
+    */
+   public void setSmokingStatus(String smokingStatus) {
+      this.smokingStatus = smokingStatus;
+   }
+   
+   /**
+    * Gets the height of the policyholder
+    *
+    * @return the height in inches
+    */
+   public double getHeight() {
+      return height;
+   }
+   
+   /**
+    * Sets the height of the policyholder
+    *
+    * @param height the height to set in inches
+    */
+   public void setHeight(double height) {
+      this.height = height;
+   }
+   
+   /**
+    * Gets the weight of the policyholder
+    *
+    * @return the weight in pounds
+    */
+   public double getWeight() {
+      return weight;
+   }
+   
+   /**
+    * Sets the weight of the policyholder
+    * 
+    * @param weight the weight to set in pounds
+    */
+   public void setWeight(double weight) {
+      this.weight = weight;
+   }
+   
+   /**
+    * Calculates the BMI of the policyholder.
+    *
+    * @return the BMI value
+    */
+   public double calculateBMI() {
+      return (weight * 703) / (height * height);
+   }
+
+}
